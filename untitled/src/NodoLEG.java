461c8991eb289a0fea7c1a381a36dba9e7c0a21b
Manuel Martín Castelao <manuel.martin.castelao@estudiantes.uva.es>
2024-03-04 17:03:43 +0100

    hola

---
+++ untitled/src/NodoLEG.java
@@ -0,0 +1,11 @@
+public class NodoLEG <E>{
+    E dato;
+    NodoLEG<E> siguiente;
+    NodoLEG(E dato){
+        this(dato, null);
+    }
+    NodoLEG(E dato, NodoLEG<E> siguiente){
+        this.dato = dato;
+        this.siguiente = siguiente;
+    }
+}
